e4f1c450fbf937b2935530e39d0a3c863831a2e3
dcproulx64 <denis.proulx64@gmail.com>
2020-03-09 21:49:02 +0000

    make it better

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
 
-app.set('port', (process.envPORT || 5000));
+app.set('port', port);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
